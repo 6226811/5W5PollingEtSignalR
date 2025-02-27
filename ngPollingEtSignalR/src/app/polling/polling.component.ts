c9b28b3dbe4d119dcd2d706ac5124c8f169610e0
6226811 <6226811@cegepmontpetit.ca>
2025-02-27 15:03:34 -0500

    PROGRES Polling fini

--- ngPollingEtSignalR/src/app/polling/polling.component.ts
+++ ngPollingEtSignalR/src/app/polling/polling.component.ts
@@ -8,8 +8,6 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { lastValueFrom } from 'rxjs';
-
-
 @Component({
   selector: 'app-polling',
   standalone: true,
@@ -29,22 +27,26 @@ export class PollingComponent implements OnInit {
     this.updateTasks();
   }
 
-  complete(id: number) {
+  async complete(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur (Contrôleur d'API)
-    
+    let request = await lastValueFrom(this.http.get<any>(this.apiUrl + "UselessTasks/Complete/" + id));
+    console.log(request);
   }
 
-  addtask() {
+  async addtask() {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur (Contrôleur d'API)
-
-    
+    let request = await lastValueFrom(this.http.post<any>(this.apiUrl + "UselessTasks/Add?taskText=" + this.taskname, null));
+    console.log(request);
 
     console.log(this.tasks);
   }
 
   async updateTasks() {
     // TODO: Faire une première implémentation simple avec un appel au serveur pour obtenir la liste des tâches
-    // TODO: UNE FOIS QUE VOUS AVEZ TESTER AVEC DEUX CLIENTS: Utiliser le polling pour mettre la liste de tasks à jour chaque seconde
-    
+    // TODO: UNE FOIS QUE VOUS AVEZ TESTÉ AVEC DEUX CLIENTS: Utilisez le polling pour mettre la liste de tasks à jour chaque seconde
+    let request = await lastValueFrom(this.http.get<any>(this.apiUrl + "UselessTasks/GetAll"));
+    console.log(request);
+    this.tasks = request;
+    //setTimeout(() => {this.updateTasks()}, 500);
   }
 }
