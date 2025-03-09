6d2e24583dc0448acdccb02a1c643376f2ac9bf5
6226811 <6226811@cegepmontpetit.ca>
2025-03-09 14:32:45 -0400

    PROGRES Progres

--- PollingEtSignalR/labo.signalr.api/Hubs/TaskHub.cs
+++ PollingEtSignalR/labo.signalr.api/Hubs/TaskHub.cs
@@ -1,4 +1,5 @@
 ﻿using labo.signalr.api.Data;
+using labo.signalr.api.Models;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,15 +19,34 @@ namespace labo.signalr.api.Hubs
             public static HashSet<string> ConnectedIds = new HashSet<string>();
         }
 
-        public async Task Connection(string connectionId)
+        public override async Task OnConnectedAsync()
         {
-            UserHandler.ConnectedIds.Add(connectionId);
-            await Clients.Caller.SendAsync("TaskList", _context.UselessTasks.ToListAsync());
-            //est-ce que tu envoies le tasklist au client avec un certain connectionid
-            //( await Clients.Client(connectionId).SendAsync("TaskList"); )
+            await base.OnConnectedAsync();
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("UserCount", UserHandler.ConnectedIds.Count);
+            await Clients.Caller.SendAsync("TaskList", _context.UselessTasks.ToList());
+        }
 
-            //ou est-ce que tu envoies le tasklist au user avec un certain userid
-            //( await Clients.User(userId).SendAsync("TaskList"); )
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception); 
+            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+            await Clients.All.SendAsync("UserCount", UserHandler.ConnectedIds.Count);
+        }
+
+        public async Task AddTask(string name)
+        {
+            _context.UselessTasks.Add(new UselessTask { Text = name });
+            await _context.SaveChangesAsync();
+            await Clients.All.SendAsync("TaskList", _context.UselessTasks.ToList());
+        }
+
+        public async Task CompleteTask(int id)
+        {
+            UselessTask task = _context.UselessTasks.Find(id);
+            task.Completed = true;
+            await _context.SaveChangesAsync();
+            await Clients.All.SendAsync("TaskList", _context.UselessTasks.ToList());
         }
     }
 }
